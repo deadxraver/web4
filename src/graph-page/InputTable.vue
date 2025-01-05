2abf9646bcff87687e1b6957691354d31d3973df
Даниил <deadxraver@yandex.ru>
2025-01-06 02:43:23 +0300

    основную логику прописал, обернул в форму, осталась отправка, канвасик (ну то есть все что уже и так умею) и добавлю где нибудь в углу инфу о пользователе

--- src/graph-page/InputTable.vue
+++ src/graph-page/InputTable.vue
@@ -1,60 +1,63 @@
 <script setup>
-import {checkY, r, x, y, yErrorText} from "@/graph-page/formElements.js";
+import {checkY, handleSubmit, r, x, y, yErrorText} from "@/graph-page/formElements.js";
 import {ref} from "vue";
+
 const pButtob = ref(null);
 </script>
 
 <template>
-  <table>
-    <tr>
-      <th>Поле</th>
-      <th>Значение</th>
-    </tr>
-    <tr>
-      <td>X</td>
-      <td>
-        <select v-model="x">
-          <option>-4</option>
-          <option>-3</option>
-          <option>-2</option>
-          <option>-1</option>
-          <option>0</option>
-          <option>1</option>
-          <option>2</option>
-          <option>3</option>
-          <option>4</option>
-        </select>
-      </td>
-    </tr>
-    <tr>
-      <td>Y</td>
-      <td>
-        <input @input="checkY(pButtob)" type="number" v-model="y" placeholder="Значение от -3 до 3">
-        <p style="color: red">{{ yErrorText }}</p>
-      </td>
-    </tr>
-    <tr>
-      <td>R</td>
-      <td>
-        <select v-model="r">
-          <option>-4</option>
-          <option>-3</option>
-          <option>-2</option>
-          <option>-1</option>
-          <option>0</option>
-          <option>1</option>
-          <option>2</option>
-          <option>3</option>
-          <option>4</option>
-        </select>
-      </td>
-    </tr>
-    <tr>
-      <td colspan="2" style="text-align: center">
-        <input ref="pButtob" class="buttob" type="submit" value="Отправить" src="img.png">
-      </td>
-    </tr>
-  </table>
+  <form @submit.prevent="handleSubmit">
+    <table>
+      <tr>
+        <th>Поле</th>
+        <th>Значение</th>
+      </tr>
+      <tr>
+        <td>X</td>
+        <td>
+          <select required v-model="x">
+            <option>-4</option>
+            <option>-3</option>
+            <option>-2</option>
+            <option>-1</option>
+            <option>0</option>
+            <option>1</option>
+            <option>2</option>
+            <option>3</option>
+            <option>4</option>
+          </select>
+        </td>
+      </tr>
+      <tr>
+        <td>Y</td>
+        <td>
+          <input required @input="checkY(pButtob)" type="number" v-model="y" placeholder="Значение от -3 до 3">
+          <p style="color: red">{{ yErrorText }}</p>
+        </td>
+      </tr>
+      <tr>
+        <td>R</td>
+        <td>
+          <select required v-model="r">
+            <option>-4</option>
+            <option>-3</option>
+            <option>-2</option>
+            <option>-1</option>
+            <option>0</option>
+            <option>1</option>
+            <option>2</option>
+            <option>3</option>
+            <option>4</option>
+          </select>
+        </td>
+      </tr>
+      <tr>
+        <td colspan="2" style="text-align: center">
+          <input ref="pButtob" class="buttob" type="submit" value="СТРЕЛЯЙ">
+        </td>
+      </tr>
+    </table>
+  </form>
 </template>
 
 <style scoped>
--- src/graph-page/formElements.js
+++ src/graph-page/formElements.js
@@ -1,4 +1,5 @@
 import {ref} from "vue";
+import 'https://cdn.jsdelivr.net/gh/cosmogicofficial/quantumalert@latest/minfile/quantumalert.js'
 
 export const x = ref(0);
 export const y = ref(0);
@@ -7,7 +8,7 @@ export const r = ref(0);
 export const yErrorText = ref('')
 
 export function checkY(pButtob) {
-	if (y.value < -3 || y.value > 3) {
+	if (y.value === '' || y.value < -3 || y.value > 3) {
 		pButtob.style = "display:none";
 		yErrorText.value = 'Значение может быть от -3 до 3';
 	}
@@ -15,4 +16,14 @@ export function checkY(pButtob) {
 		pButtob.style = "display:visible"
 		yErrorText.value = '';
 	}
+}
+
+export function handleSubmit() {
+	if (yErrorText.value) {
+		Qual.error("Ошибка", "Проверьте правильность ввода всех полей формы");
+		return false;
+	}
+	// TODO: отправка формы
+	Qual.success("урааа","тут скоро форма будет отправляться");
+	return false;
 }
